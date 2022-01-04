a5ca8d94bc8019da2a0b3433ca14aa0567b4e01b
gokhanyalinbas <37847103+gokhanyalinbas@users.noreply.github.com>
2022-01-04 16:38:04 +0300

    Delete mancalagame/mancalagame directory

--- mancalagame/mancalagame/src/main/java/com/bol/mancala/mancalagame/exception/LoginSessionExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.bol.mancala.mancalagame.exception;
-
-public class LoginSessionExpiredException extends RuntimeException {
-    public LoginSessionExpiredException(String s) {
-        super(s);
-    }
-}
--- mancalagame/mancalagame/src/main/java/com/bol/mancala/mancalagame/exception/UnexpectedMoveException.java
+++
@@ -1,7 +0,0 @@
-package com.bol.mancala.mancalagame.exception;
-
-public class UnexpectedMoveException extends RuntimeException {
-    public UnexpectedMoveException(String s) {
-        super(s);
-    }
-}
